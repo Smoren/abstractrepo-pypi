f77f5ef68397add58b146ec717cca8ea1de6a6d1
Smoren <ofigate@gmail.com>
2025-07-28 20:13:05 +0300

    Minor changes.

--- tests/order_test.py
+++ tests/order_test.py
@@ -9,7 +9,7 @@ from tests.providers.order import data_provider_for_news_order
 
 
 @pytest.mark.parametrize("test_case", data_provider_for_news_order())
-def test_filter(test_case: Tuple[List[News], OrderOptions, List[News]]):
+def test_order(test_case: Tuple[List[News], OrderOptions, List[News]]):
     input_news, order_options, expected = test_case
     repo = ListBasedNewsRepository(input_news)
     actual = repo.get_collection(order_options=order_options)
